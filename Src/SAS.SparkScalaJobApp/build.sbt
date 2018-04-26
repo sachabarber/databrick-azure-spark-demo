4207f6e2e4109e7e5f55924cfd76dbb1bb4fe158
sacha barber <sbarber@nephilaadvisors.co.uk>
2018-04-26 07:27:58 +0100

    Adding Scala project

--- Src/SAS.SparkScalaJobApp/build.sbt
+++ Src/SAS.SparkScalaJobApp/build.sbt
@@ -5,4 +5,11 @@ version := "1.0"
 scalaVersion := "2.11.8"
 
 assemblyJarName in assembly := "SAS.SparkScalaJobApp.jar"
+
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "2.3.0" % "provided",
+  "org.apache.spark" % "spark-sql_2.11" % "2.3.0" % "provided"
+  //"org.apache.hadoop" %% "hadoop-core" % "1.2.1" % "provided"
+)
         
--- Src/SAS.SparkScalaJobApp/src/main/scala/SparkApp.scala
+++ Src/SAS.SparkScalaJobApp/src/main/scala/SparkApp.scala
@@ -1,3 +1,24 @@
+import org.apache.spark.sql.SparkSession
+
 object SparkApp extends App {
-  Console.println("Hello World")
+
+  Console.println("===== Starting =====")
+
+  val multiplier = 2
+  Console.println(s"multiplier is set to $multiplier")
+
+  val session = SparkSession.builder().getOrCreate()
+  val sparkContext = session.sparkContext
+  val sqlContext = session.sqlContext
+
+  import sqlContext.implicits._
+
+  val data = Array(1, 2, 3, 4, 5)
+  val dataRdd = sparkContext.parallelize(data)
+  val mappedRdd = dataRdd.map(_ * multiplier)
+  val df = mappedRdd.toDF
+
+  df.show()
+
+  Console.println("===== Done =====")
 }
